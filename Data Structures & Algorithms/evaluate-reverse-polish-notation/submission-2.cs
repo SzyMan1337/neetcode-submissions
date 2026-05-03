7a1b0848a90b2e395182af1c267cb05f60eaa7f8
Mateusz Szymański <73383275+SzyMan1337@users.noreply.github.com>
2026-05-03 06:44:33 +0200

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public int EvalRPN(string[] tokens) {
+        int result = 0;
+        var stack = new Stack<int>();
+
+        foreach(var token in tokens)
+        {
+            if(int.TryParse(token, out int num))
+            {
+                stack.Push(num);
+            }
+            else
+            {
+                var num1 = stack.Pop();
+                var num2 = stack.Pop();
+                
+                switch(token)
+                {
+                    case "+":
+                        result = num2+num1;
+                        break;
+                    case "-":
+                        result = num2-num1;
+                        break;
+                    case "/":
+                        result = num2/num1;
+                        break;
+                    case "*":
+                        result = num2*num1;
+                        break;
+                }
+                stack.Push(result);
+            }
+        }
+
+        return stack.Pop();
+    }
+}
