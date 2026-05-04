22bd2645b033bcbbe857ba5e0896f79e3dfd957d
Mateusz Szymański <73383275+SzyMan1337@users.noreply.github.com>
2026-05-04 17:19:20 +0200

    Add: find-minimum-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int FindMin(int[] nums) {
+        int n = nums.Length;
+        int left = 0, right = n - 1;
+
+        while(left < right)
+        {
+            int mid = (left + right) / 2;
+            if(nums[mid] > nums[right])
+            {
+                left = mid +1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+
+        return Math.Min(nums[left], nums[right]);
+    }
+}
