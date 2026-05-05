be01c187305c67b6d26062cf1fcf17d69652f4d2
Mateusz Szymański <73383275+SzyMan1337@users.noreply.github.com>
2026-05-05 19:59:15 +0200

    Add: longest-repeating-substring-with-replacement - submission-10

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-10.cs
@@ -0,0 +1,31 @@
+public class Solution
+{
+    public int CharacterReplacement(string s, int k)
+    {
+        var characters = new Dictionary<char, int>();
+        int l = 0, longest = 0, maxf = 0;
+
+        for (int r = 0; r < s.Length; r++)
+        {
+            if (!characters.ContainsKey(s[r]))
+            {
+                characters.Add(s[r], 1);
+            }
+            else
+            {
+                characters[s[r]]++;
+            }
+            maxf = Math.Max(maxf, characters[s[r]]);
+
+            while ((r - l + 1) - maxf > k)
+            {
+                characters[s[l]]--;
+                l++;
+            }
+
+            longest = Math.Max(longest, r - l + 1);
+        }
+
+        return longest;
+    }
+}
