3aab437d8e1efb86db9f6090660bc5ffa54583f2
Mateusz Szymański <73383275+SzyMan1337@users.noreply.github.com>
2026-05-09 13:15:03 +0200

    Add: kth-largest-element-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+        var heap = new PriorityQueue<int, int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            heap.Enqueue(nums[i], nums[i]);
+            if (heap.Count > k) {
+                heap.Dequeue();
+            }
+        }
+        return heap.Peek();
+    }
+}
