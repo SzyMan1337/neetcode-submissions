44a15f44e36115df4abe88f5403a654cd1db357b
Mateusz Szymański <73383275+SzyMan1337@users.noreply.github.com>
2026-05-09 07:14:58 +0200

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.cs
@@ -0,0 +1,28 @@
+public class Solution
+{
+    public List<List<int>> CombinationSum(int[] nums, int target)
+    {
+        var result = new List<List<int>>();
+        Array.Sort(nums);
+        Backtract(result, nums, 0, new List<int>(), target);
+        return result;
+    }
+
+    public void Backtract(List<List<int>> result, int[] nums, int index, List<int> currentList, int currentTarget)
+    {
+        if (currentTarget == 0)
+        {
+            result.Add([.. currentList]);
+            return;
+        }
+        if (index == nums.Length || nums[index] > currentTarget)
+        {
+            return;
+        }
+
+        currentList.Add(nums[index]);
+        Backtract(result, nums, index, currentList, currentTarget - nums[index]);
+        currentList.RemoveAt(currentList.Count - 1);
+        Backtract(result, nums, index + 1, currentList, currentTarget);
+    }
+}
