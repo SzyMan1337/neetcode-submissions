0daaf6be27adf02e7bbb8d118a4cd207c3de8ced
Mateusz Szymański <73383275+SzyMan1337@users.noreply.github.com>
2026-05-11 20:19:12 +0200

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int HammingWeight(uint n) {
+        int count = 0;
+        
+        for(int i =0; i<32;i++){
+            if((n >> i & 1) > 0){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
