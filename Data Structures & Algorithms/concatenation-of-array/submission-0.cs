7536773e69684b99897ed4e0347168f46c7986ab
Mateusz Szymański <73383275+SzyMan1337@users.noreply.github.com>
2026-05-17 16:16:51 +0200

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        int n = nums.Length;
+
+        var ans = new int[2*n];
+
+        for(int i=0; i<n;i++){
+            ans[i] = ans[n+i] = nums[i];
+        }
+
+        return ans;
+    }
+}
