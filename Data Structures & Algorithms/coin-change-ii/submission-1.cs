f58f4e857fb112612e6ef7e412090d590cc7410c
Mateusz Szymański <73383275+SzyMan1337@users.noreply.github.com>
2026-05-19 20:18:14 +0200

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int Change(int amount, int[] coins) {
+        int n = coins.Length;
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for(int i =n-1; i>=0; i--){
+            int[] nextDP = new int[amount + 1];
+            nextDP[0] = 1;
+
+            for (int a = 1; a <= amount; a++) {
+                nextDP[a] = dp[a];
+                if (a - coins[i] >= 0) {
+                    nextDP[a] += nextDP[a - coins[i]];
+                }
+            }
+            dp = nextDP;
+        }
+
+        return dp[amount];
+    }
+}
