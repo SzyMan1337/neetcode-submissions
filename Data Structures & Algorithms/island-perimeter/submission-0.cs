c3d757dd7568f185e124cf5ecac36479bbbb4c71
Mateusz Szymański <73383275+SzyMan1337@users.noreply.github.com>
2026-05-25 17:11:23 +0200

    Add: island-perimeter - submission-0

---
+++ Data Structures & Algorithms/island-perimeter/submission-0.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int IslandPerimeter(int[][] grid) {
+        int m = grid.Length;
+        int n = grid[0].Length;
+        int res = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    if (i + 1 >= m || grid[i + 1][j] == 0) res++;
+                    if (j + 1 >= n || grid[i][j + 1] == 0) res++;
+                    if (i - 1 < 0 || grid[i - 1][j] == 0) res++;
+                    if (j - 1 < 0 || grid[i][j - 1] == 0) res++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
