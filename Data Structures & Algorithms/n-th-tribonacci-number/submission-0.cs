bf48db5fbae62af3816df34300f74f0d4071e3b6
Mateusz Szymański <73383275+SzyMan1337@users.noreply.github.com>
2026-05-25 16:35:51 +0200

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int Tribonacci(int n) {
+        int[] t = {0, 1, 1};
+        if (n < 3) return t[n];
+
+        for (int i = 3; i <= n; i++) {
+            t[i % 3] = t[0] + t[1] + t[2];
+        }
+
+        return t[n % 3];
+    }
+}
