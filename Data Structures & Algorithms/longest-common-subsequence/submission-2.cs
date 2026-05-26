b20808848e7df41c4576372f0deec9227f884d5f
Mateusz Szymański <73383275+SzyMan1337@users.noreply.github.com>
2026-05-26 09:19:25 +0200

    Add: longest-common-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-2.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int LongestCommonSubsequence(string text1, string text2) {
+        int n = text1.Length;
+        int m = text2.Length;
+
+        var dp = new int[n+1,m+1];
+
+        for(int i =m-1; i>=0;i--){
+            char c = text2[i];
+
+            for(int j = n-1; j>=0;j--){
+                if(c == text1[j]){
+                    dp[j,i] = dp[j+1,i+1] +1;
+                }
+                else
+                {
+                    dp[j,i] = Math.Max(dp[j,i+1], dp[j+1,i]);
+                }
+            }
+        }
+
+        return dp[0,0];
+    }
+}
